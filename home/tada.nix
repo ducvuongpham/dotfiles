09a48666f1bdfb6d637fc3902f5f0df751677acb
ducvuongpham <ducvuongpham2004@gmail.com>
2026-05-27 21:58:38 +0900

    dbeaver: re-enable now that sops can decrypt on tada-mbp

--- home/tada.nix
+++ home/tada.nix
@@ -10,10 +10,5 @@
 
   programs.home-manager.enable = true;
 
-  # Disabled until secrets/dbeaver/*.enc are re-encrypted to include
-  # tada_mbp's age recipient. On pc391 run:
-  #   sops updatekeys secrets/dbeaver/credentials-config.enc
-  #   sops updatekeys secrets/dbeaver/data-sources.enc
-  # then commit + push. Pull here and flip this back to true.
-  local.dbeaver.enable = false;
+  local.dbeaver.enable = true;
 }
